02a7da21a4fe7c65f7b17274976b18f4605a9e81
Samray <samray.tian@gmail.com>
2023-12-01 22:46:33 +0800

    feat(colorscheme):  make neovim background transparent with tokyonight color scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+
+  -- Configure tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "moon",
+      light_style = "day",
+      transparent = true,
+    },
+  },
+}
